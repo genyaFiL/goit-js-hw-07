ea9ed6ae2f3ca0c713c0f1ac6c0ef632c8819a87
genyaFiL <genyafil@gmail.com>
2023-03-06 19:31:35 +0200

    add Task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const createMarkupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+`
+  )
+  .join("");
+galleryContainer.insertAdjacentHTML("afterbegin", createMarkupGallery);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
