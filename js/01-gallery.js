be326ffa46f66d09ea1ee24ad6a2de063485f69c
genyaFiL <genyafil@gmail.com>
2023-03-06 18:04:12 +0200

    add Task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+const createMarkupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", createMarkupGallery);
+galleryContainer.addEventListener("click", outputEnlargedImage);
+
+function outputEnlargedImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = event.target.dataset.source;
+  console.log(selectedImg);
+
+  const modalWindow = basicLightbox.create(
+    `
+		<img width="1400" height="900" src="${selectedImg}">
+	`
+  );
+  modalWindow.show();
+
+  const handleClickModalWindow = (event) => {
+    event.preventDefault();
+    if (event.code === "Escape") {
+      modalWindow.close();
+      console.log("Escape pressed");
+      document.removeEventListener("keydown", handleClickModalWindow);
+    }
+  };
+
+  document.addEventListener("keydown", handleClickModalWindow);
+}
 
 console.log(galleryItems);
